58028d1611bf1620061bee03371e2be9e9f34657
Adenilson A. da Silva <adenilson@topsys.com>
2022-04-02 16:59:27 -0300

    add procfile and enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  if (process.env.DEVELOPMENT_MODE === 'TRUE') app.enableCors();
+  app.enableCors();
 
   await app.listen(process.env.PORT || 3001);
 }
